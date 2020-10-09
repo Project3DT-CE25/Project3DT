a07b4ccfbcd6ae6aeb0c0da6c54a7e5d5daf703d
pasure <56163008+pasure@users.noreply.github.com>
2020-10-09 14:58:48 +0700

    เทสประโยคเพิ่มเติม

--- voice.py
+++ voice.py
@@ -8,4 +8,6 @@ print(vcs.Item (speaker_number) .GetAttribute ("Name")) # speaker name
 spk.Voice
 spk.SetVoice(vcs.Item(speaker_number)) # set voice (see Windows Text-to-Speech settings)
 spk.Speak("35.5องศา")
-
+spk.Speak("อุณหภูมิเกิน 37.5องศา กรุณาลองใหม่อีกครั้ง")
+spk.Speak("กรุณาลองใหม่อีกครั้ง")
+spk.Speak("ผ่านครับ")
