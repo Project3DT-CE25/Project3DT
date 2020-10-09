66249339246c3b31a92a9c17d59aa3eadb65b505
pasure <56163008+pasure@users.noreply.github.com>
2020-10-09 14:55:26 +0700

    เทสเสียง

--- voice.py
+++ voice.py
@@ -1,13 +1,11 @@
-# pip install pypiwin32
 import win32com.client as wincl
+TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech_OneCore\Voices\Tokens\MSTTS_V110_thTH_Pattara"
+speaker_number = 1
+spk = wincl.Dispatch("SAPI.SpVoice")
+vcs = spk.GetVoices()
+SVSFlag = 11
+print(vcs.Item (speaker_number) .GetAttribute ("Name")) # speaker name
+spk.Voice
+spk.SetVoice(vcs.Item(speaker_number)) # set voice (see Windows Text-to-Speech settings)
+spk.Speak("35.5องศา")
 
-def demo_win10():
-    syn = wincl.Dispatch('SAPI.SpVoice') # use Windows SAPI (Speech API)
-    syn.Rate = 0 # normal speed = 0
-    syn.Volume = 100 # max volume = 100
-    s = 'I love Thailand'
-    print(s)
-    syn.Speak(s)
-
-demo_win10()
-print("Helloworld2")
