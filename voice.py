10ee68ee858134323761447f463c222e631bd051
Sila <56313448+ChSila@users.noreply.github.com>
2020-10-09 15:19:28 +0700

    Update voice.py
    Condition no Mask

--- voice.py
+++ voice.py
@@ -11,3 +11,11 @@ spk.Speak("35.5องศา")
 spk.Speak("อุณหภูมิเกิน 37.5องศา กรุณาลองใหม่อีกครั้ง")
 spk.Speak("กรุณาลองใหม่อีกครั้ง")
 spk.Speak("ผ่านครับ")
+import pyttsx3
+engine = pyttsx3.init()
+TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_thTH_Pattara"
+engine.setProperty('volume', 1)  # Volume 0-1
+engine.setProperty('rate', 180)  #148
+engine.setProperty('voice', TH_voice_id)
+engine.say('กรุณาสวมหน้ากากอนามัยด้วยครับ')
+engine.runAndWait()
