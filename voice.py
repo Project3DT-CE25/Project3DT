fa11078967a66cfadf718670fa31fa640110e5ae
Sila <56313448+ChSila@users.noreply.github.com>
2021-03-18 20:20:18 +0700

    Update voice.py

--- voice.py
+++ voice.py
@@ -4,7 +4,7 @@ speaker_number = 1
 spk = wincl.Dispatch("SAPI.SpVoice")
 vcs = spk.GetVoices()
 SVSFlag = 11
-print(vcs.Item (speaker_number) .GetAttribute ("Name")) # speaker name
+print(vcs.Item (speaker_number) .GetAttribute ("Name")) # speaker name ?
 spk.Voice
 spk.SetVoice(vcs.Item(speaker_number)) # set voice (see Windows Text-to-Speech settings)
 spk.Speak("35.5องศา")
